1174cc5b70b8a6fe6dfff0ade3ab72d4dc1e7e68
bolshu <bolshu@MacBook-Pro-bolshu.local>
2021-08-02 17:24:42 +0300

    upd status codes

--- index.js
+++ index.js
@@ -8,30 +8,38 @@ app.use(
     extended: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 /**
  * @typedef {Object} Request
  * @property {string} name
  * @property {string} email
  * @property {string} phone
- * 
+ *
  * @typedef {Object} Response
  * @property {number} status
  * @property {string} message
- * 
+ *
  * @param {Request} [req.body]
- * 
+ *
  * @return {Response}
  */
 app.post("/registration", (req, res) => {
   if (req.body.email || req.body.phone) {
+    res.status(200);
     res.send({
-      status: 200,
       message: "Все хорошо, вы зареганы!",
     });
   } else {
+    res.status(400);
     res.send({
-      status: 400,
       message: "Введите электронную почту или телефон",
     });
   }
